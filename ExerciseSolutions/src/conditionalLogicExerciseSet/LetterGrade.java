b0795bf75248c55824f53bf3a1f97fe9b55772a5
Daniel <dagbaji@g.emporia.edu>
2019-03-28 09:22:37 -0500

    This code contains all the solutions to the variable and Math Exercise Set, Conditional Logical Exercise Set, and the Functions Exercise Set Problems for Claim Academy.

--- ExerciseSolutions/src/conditionalLogicExerciseSet/LetterGrade.java
+++ ExerciseSolutions/src/conditionalLogicExerciseSet/LetterGrade.java
@@ -32,4 +32,50 @@ package conditionalLogicExerciseSet;
  */
 public class LetterGrade {
     
+    public static void main (String [] args){
+        
+        // declaring and initializing the gradeNumber value;
+        
+        int gradeNumber = 76;
+        
+        // creating a range of numbers
+        switch ((72 <= gradeNumber && gradeNumber <= 79 ) ? 72 :
+                (64 <= gradeNumber && gradeNumber <= 71 ) ? 72 :
+                (80 <= gradeNumber && gradeNumber <= 99)? 80:
+                (0 <= gradeNumber && gradeNumber <= 63) ? 72 :
+                (90 <= gradeNumber && gradeNumber <= 100) ? 90 : 100)
+                                                        
+                                                                         {
+            
+            case 90:
+            case 100:
+                System.out.println("A is your grade");
+                break;
+            case 80:
+            case 99:
+                System.out.println("B is your grade");
+                break;
+            case 72:
+            case 79:
+                System.out.println("C is your grade");
+                break;
+            case 64:
+            case 71:
+                System.out.println("D is your grade");
+                break;
+            case 0:
+            case 63:
+                System.out.println("F is your grade");
+                break;
+            default:
+                    System.out.println("You got an invalide grade");
+                    break;
+    }
+        
+        
+    }
+    
+    
+    
+    
 }
