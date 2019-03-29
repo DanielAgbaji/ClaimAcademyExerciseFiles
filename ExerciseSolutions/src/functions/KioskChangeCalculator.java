4ac9c9f1e4f3aad638db28f2bf73d4aa7f5e823c
Daniel <dagbaji@g.emporia.edu>
2019-03-29 15:42:33 -0500

    This code contains all the solutions to the variable and Math Exercise Set, Conditional Logical Exercise Set, and the Functions Exercise Set Problems for Claim Academy.

--- ExerciseSolutions/src/functions/KioskChangeCalculator.java
+++ ExerciseSolutions/src/functions/KioskChangeCalculator.java
@@ -10,16 +10,12 @@ people use cash to pay for an item, the kiosk needs to tell the cashier how much
 change should be given back to the customer. 
 You are developing a simple calculator
  that will compute the right amount and most 
-e
-ffi
-cient denominations of change for a speci
-fi
-c purchase. 
+efficient denominations of change for a specific purchase. 
 returnChange(18, 17.18)
 RESULT:    Quarters: 3 
   Dimes: 0 
   Nickels: 1 
-  Pennies:
+  Pennies: 2
 */
 package functions;
 
@@ -28,5 +24,34 @@ package functions;
  * @author danielagbaji
  */
 public class KioskChangeCalculator {
+    public static void main (String [] args){
+        
+        int cash = 18;
+        
+        double prize = 17.18;
+        
+        double returnChange = getReturnChange(cash, prize);
+        
+        System.out.println(returnChange);
+        
+    }
+    
+    public static double getReturnChange(int cash, double prize){
+        
+        
+        double change = (double)(cash - prize)*100;
+        
+        double returnChange = change/2;
+        
+        if (returnChange == 41 ){
+          
+            System.out.println ("Your change is "  +returnChange);
+            
+            
+        }
+        
+       return returnChange; 
+        
+    }
     
 }
